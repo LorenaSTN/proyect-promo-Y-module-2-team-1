4200bff8f3fa1f7f367fb6bf51c702c1e6f11a55
griseldagomez <grinoe123@gmail.com>
2024-06-26 12:31:34 +0200

    js form

---
+++ src/js/form.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+/* 
+El formulario se mostrarà en la tarjeta
+1-Crear formulario
+2-Recoger la info de la usuaria cuando escriba en el input - definir variable input + crear evento.(input/keydown) - {event.target.value}
+3-La info se previzualiza en la pantalla -definir las variables de la tarjeta. 
+*/
+
