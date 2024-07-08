6628ae31345f294b82f359881041b86684d6475a
LorenaSTN <lorenstn@hotmail.com>
2024-07-08 19:55:32 +0200

    handleCollapsable

--- src/js/form.js
+++ src/js/form.js
@@ -12,13 +12,6 @@ const dataCard = {
   photo: "",
 };
 
-/* 
-El formulario se mostrarà en la tarjeta
-1-Crear formulario
-2-Recoger la info de la usuaria cuando escriba en el input - definir variable input + crear evento.(input/keydown) - {event.target.value}
-3-La info se previzualiza en la pantalla -definir las variables de la tarjeta. 
-4-Iconos: Cuando la usuari@ haga click se redirigirá a un enlace.******
-*/
 
 const formName = document.querySelector(".js-form-name");
 const cardName = document.querySelector(".js-card-name");
@@ -35,17 +28,7 @@ const cardGithub = document.querySelector(".js-card-github");
 const formPhoto = document.querySelector(".js-form-photo img");
 const cardPhoto = document.querySelector(".js-card-image");
 
-/*  Si la usuaria escribe en el input de nombre, se ve visualizado en el preview card (en el nombre),
-Si la usuaria escribe en el input de job/trabajo, se ve visualizado en el preview card (en el job/trabajo)
-.....
 
-=== / contenido y tipo de dato
-== / contenido
-
-
-
-iconos- href= 
-*/
 
 const buttonArrow = document.querySelector(".js-button-arrow");
 const dropdown = document.querySelector(".js-dropdown");
@@ -53,40 +36,37 @@ const buttonArrow2 = document.querySelector(".js-button-arrow2");
 const dropdown2 = document.querySelector(".js-dropdown2");
 const buttonArrow3 = document.querySelector(".js-button-arrow3");
 const dropdown3 = document.querySelector(".js-dropdown3");
+const collapsableHeaders = document.querySelectorAll(".js-collapsable-title")
 
 
-// dropdown.classList.add("hiden");
 dropdown2.classList.add("hiden");
 dropdown3.classList.add("hiden");
-// function de ocultar- no se porque no funciona?
-function handleHidingPalettes(event) {
-  event.preventDefault();
-  dropdown.classList.toggle("hiden");
-  buttonArrow.classList.toggle("rotate");
-  dropdown2.classList.add("hiden");
-  dropdown3.classList.add("hiden");
-}
-buttonArrow.addEventListener("click", handleHidingPalettes);
 
-function handleHidingForm(event) {
-  event.preventDefault();
-  dropdown2.classList.toggle("hiden");
-  buttonArrow2.classList.toggle("rotate");
-  dropdown.classList.add("hiden");
-  dropdown3.classList.add("hiden");
-}
 
-buttonArrow2.addEventListener("click", handleHidingForm);
+function handleCollapsable(event){
+  const clickedLegend = event.currentTarget;
+  
+  if (clickedLegend.classList.contains("js-fieldset-palettes")){
+    dropdown.classList.toggle("hiden");
+    dropdown2.classList.add("hiden");
+    dropdown3.classList.add("hiden");
+  }else if (clickedLegend.classList.contains("js-fieldset-fill")){
+    dropdown2.classList.toggle("hiden");
+    dropdown.classList.add("hiden");
+    dropdown3.classList.add("hiden");
+  }else if (clickedLegend.classList.contains ("js-fieldset-share")){
+    event.preventDefault();
+    dropdown3.classList.toggle("hiden");
+    buttonArrow3.classList.toggle("rotate");
+    dropdown.classList.add("hiden");
+    dropdown2.classList.add("hiden");
+  };
+};
 
-function handleHidingShare(event) {
-  event.preventDefault();
-  dropdown3.classList.toggle("hiden");
-  buttonArrow3.classList.toggle("rotate");
-  dropdown.classList.add("hiden");
-  dropdown2.classList.add("hiden");
-}
 
-buttonArrow3.addEventListener("click", handleHidingShare);
+for(const collapsableHeader of collapsableHeaders){
+  collapsableHeader.addEventListener("click", handleCollapsable)
+};
 
 ///////
 
@@ -121,36 +101,6 @@ inputsFormFill.addEventListener("input", (event) => {
 
 });
 
-//cardPhoto.src = `"${formPhoto.value}"`;
-//dataCard.photo = event.target.value;        /
-// const cardIconEmail = document.querySelector(".js-icon-email");
-
-// cardIconEmail.addEventListener ("click", (event) =>{
-//     event.preventDefault()
-//         event.target.id === "email" ;
-//         const emailInputValue = formEmail.value;
-//         emailInputValue = ` mailto:${emailInputValue}`;
-//         window.location.href = emailInputValue;
-//     }
-// )
-
-// else if (event.target.id === "email"){
-//         const emailInputValue = formEmail.value;
-//         emailInputValue = `mailto:${emailInputValue}`;
-//         window.location.href = emailInputValue;
-// cardEmail.href = `mailto:${formEmail.value}`; //cambie el innerHTML por (herf =mailto.)
-
-// formName.addEventListener ("input" , (event)=>{
-//     const value = event.target.value;
-//     cardName.innerHTML = value;
-// } )
-
-/* formJob.addEventListener ("input" , (event)=> {
-    const value1 = event.target.value;
-    cardJob.innerHTML = value1;
-}) */
-
-//Section reset button//
 
 const resetButton = document.querySelector(".js-button-reset");
 const inputsForm = document.querySelectorAll(".js-form-fill input");
